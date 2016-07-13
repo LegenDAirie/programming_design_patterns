b7a11d6fd1d9e16cecbf1019e2ba36bd9035b914
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-13 15:31:28 -0700

    implemented the exercise file example from scratch

--- behavioral_patterns/command_pattern/from_scratch/taskRepo.js
+++ behavioral_patterns/command_pattern/from_scratch/taskRepo.js
@@ -1,83 +1,93 @@
 var _ = require('../../../utility/lodash');
 
-var repo = (function(){
-
+var taskRepo = (function() {
   var tasks = {};
-
   var commands = [];
 
-  var pushCommands = function(command) {
+  var save = function(task) {
+    tasks[task.id] = task;
+    console.log('saving ' + task.name + ' to the database');
+  };
+
+  var complete = function(task) {
+    console.log('completing ' + task.name);
+  };
+
+  var logExecution = function(command) {
     commands.push(command);
   };
 
-  return {
-    save: function(task) {
-      tasks[task.id] = task;
-      console.log('saving ' + task.name + ' to the database');
-    },
-
-    complete: function(task) {
-      console.log('completing ' + task.name);
-    },
-
-    replay: function() {
-      _.forEach(commands, function(command) {
-        repo.executeNoLog(command.name, command.obj);
-      });
-    },
-    pushCommands: pushCommands,
+  var showTasks = function() {
+    return tasks;
   };
+
+  var eraseTasks = function() {
+    tasks = {};
+  };
+
+  var rebuildDatabase = function () {
+    _.forEach(commands, function(command) {
+      taskRepo.executeNoLog(command.name, command.obj);
+    });
+  };
+
+
+  return {
+    save: save,
+    complete: complete,
+    logExecution: logExecution,
+    showTasks: showTasks,
+    eraseTasks: eraseTasks,
+    rebuildDatabase: rebuildDatabase
+  }
 })();
 
-repo.executeNoLog = function(name) {
+taskRepo.execute = function(commandName) {
   var args = Array.prototype.slice.call(arguments, 1);
 
-  if(repo[name]) {
-    return repo[name].apply(repo, args);
-  }
-
-  return false;
-};
-
-repo.execute = function(name) {
-  var args = Array.prototype.slice.call(arguments, 1);
-
-  repo.pushCommands({
-    name: name,
+  taskRepo.logExecution({
+    name: commandName,
     obj: args[0]
   });
 
-  if(repo[name]) {
-    return repo[name].apply(repo, args);
+  if(taskRepo[commandName]) {
+    taskRepo[commandName].call(taskRepo, args[0]);
+  }
+
+  return false;
+};
+taskRepo.executeNoLog = function(commandName) {
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  if(taskRepo[commandName]) {
+    taskRepo[commandName].call(taskRepo, args[0]);
   }
 
   return false;
 };
 
-repo.execute('save', {
-  name: "task1",
-  id: 1,
-});
-repo.execute('save', {
-  name: "task2",
-  id: 2,
-});
-repo.execute('save', {
-  name: "task3",
-  id: 3,
-});
-repo.execute('save', {
-  name: "task4",
-  id: 4,
-});
-repo.execute('save', {
-  name: "task5",
-  id: 5,
+taskRepo.execute('save', {
+  name: 'task1',
+  id: 1
 });
 
-console.log(repo.tasks);
-repo.tasks = {};
-console.log(repo.tasks);
+taskRepo.execute('save', {
+  name: 'task2',
+  id: 2
+});
 
-repo.replay();
-console.log(repo.tasks);
+taskRepo.execute('save', {
+  name: 'task3',
+  id: 3
+});
+
+taskRepo.execute('save', {
+  name: 'task4',
+  id: 4
+});
+
+console.log(taskRepo.showTasks());
+taskRepo.eraseTasks();
+console.log(taskRepo.showTasks());
+taskRepo.rebuildDatabase();
+console.log(taskRepo.showTasks());
